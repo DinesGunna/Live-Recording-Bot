e1d579bfd4cda4f382547890e0d153501ec91a4a
DinesGunna <91423674+DinesGunna@users.noreply.github.com>
2023-01-14 19:39:26 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,12 +1,12 @@
 import os
 
 # Bot token from @botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("5785157567:AAH1pfBnLM3khKHM79Pq1xKoWtToEFG4CDU")
 # From my.telegram.org/
-API_ID = int(os.environ.get("API_ID", 0))
-API_HASH = os.environ.get("API_HASH")
+API_ID = int(os.environ.get("9976721", 0))
+API_HASH = os.environ.get("3ef17a8cdb938335bd8ba292e6d816aa")
 # For /log cmd
-OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "1204927413").split(" ")]
+OWNER_ID = [int(i) for i in os.environ.get("OWNER_ID", "1956698956").split(" ")]
 # No time limit for this users
 AUTH_USERS = [int(i) for i in os.environ.get("AUTH_USERS", "1204927413").split(" ")]
 # Time gap after each request (in seconds)
